e241bde27f16f34c05784941ff022b8a287beff9
Alejandro <alejandro.arancibia.aranda@gmail.com>
2025-09-20 03:18:25 -0300

    feat: add scripts to test the database

---
+++ scripts/clearDatabase.js
@@ -0,0 +1,12 @@
+import { prisma } from '../src/database/connection.js'
+
+const clearDatabase = async () => {
+  try {
+    console.log('🗑️  Cleaning database')
+    await prisma.user.deleteMany()
+  } catch (error) {
+    console.error('❌ Error cleaning the database', error)
+  }
+}
+
+clearDatabase()
---
+++ scripts/createUsers.js
@@ -0,0 +1,28 @@
+import { prisma } from '../src/database/connection.js'
+
+const userData = [
+  { balance: 150, currency: 'USD' },
+  { balance: 100, currency: 'USD' },
+  { balance: 0, currency: 'USD' }
+]
+
+const createUsers = async () => {
+  try {
+    console.log('🚀 Creating new users')
+
+    const users = await Promise.all(
+      userData.map(data => prisma.user.create({ data }))
+    )
+    console.table(
+      users.map(user => ({
+        ID: user.id,
+        Balance: user.balance,
+        Currency: user.currency
+      }))
+    )
+  } catch (error) {
+    console.error('❌ Error creating users', error)
+  }
+}
+
+createUsers()
