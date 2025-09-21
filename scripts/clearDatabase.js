ccb3ee55a5b551361edaba1ab668b91f468d8c4f
Alejandro <alejandro.arancibia.aranda@gmail.com>
2025-09-21 16:21:08 -0300

    fix: script clear database now it works

--- scripts/clearDatabase.js
+++ scripts/clearDatabase.js
@@ -3,6 +3,8 @@ import { prisma } from '#src/database/connection.js'
 const clearDatabase = async () => {
   try {
     console.log('🗑️  Cleaning database')
+    await prisma.recharge.deleteMany()
+    await prisma.transaction.deleteMany()
     await prisma.user.deleteMany()
   } catch (error) {
     console.error('❌ Error cleaning the database', error)
